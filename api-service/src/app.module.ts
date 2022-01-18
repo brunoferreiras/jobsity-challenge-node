8c60002e11c3683772245fbb1f7e2a5489a788d0
Bruno Ferreira <bruno.ferreira@fortics.com.br>
2022-01-18 19:36:55 -0300

    feat: create auth module

--- api-service/src/app.module.ts
+++ api-service/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 import configuration from './config/configuration';
 
 @Module({
@@ -29,7 +30,8 @@ import configuration from './config/configuration';
         }
       },
     }),
-    UsersModule
+    UsersModule,
+    AuthModule
   ],
   controllers: [],
   providers: [],

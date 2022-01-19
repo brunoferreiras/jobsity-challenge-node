dc980d7cc0f6c98bf759697ea7b1d37f1181430c
Bruno Ferreira <bruno.ferreira@fortics.com.br>
2022-01-18 21:28:11 -0300

    test: create getById in repository

---
+++ api-service/src/users/users.repository.spec.ts
@@ -0,0 +1,47 @@
+import { Test, TestingModule } from "@nestjs/testing"
+import { mockUser } from "../../test/helpers"
+import { UserEntity } from "./entities/user.entity"
+import { UsersRepository } from "./users.repository"
+
+describe('UsersRepository', () => {
+  let repository: UsersRepository
+  let userMock: UserEntity
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UsersRepository],
+    }).compile()
+    repository = module.get<UsersRepository>(UsersRepository)
+    userMock = mockUser()
+  })
+
+  it('should be defined', () => {
+    expect(repository).toBeDefined()
+  })
+
+  describe('getByEmail()', () => {
+    it('should be called findOne with correct params', async () => {
+      repository.findOne = jest.fn().mockResolvedValue({})
+      await repository.getByEmail(userMock.email)
+      expect(repository.findOne).toBeCalledWith({ email: userMock.email })
+    })
+
+    it('should be returns when findOne returns', async () => {
+      repository.findOne = jest.fn().mockResolvedValue(userMock)
+      expect(await repository.getByEmail('any_email')).toEqual(userMock)
+    })
+  })
+
+  describe('getById()', () => {
+    it('should be called findOne with correct params', async () => {
+      repository.findOne = jest.fn().mockResolvedValue({})
+      await repository.getById('any_id')
+      expect(repository.findOne).toBeCalledWith('any_id')
+    })
+
+    it('should be returns when findOne returns', async () => {
+      repository.findOne = jest.fn().mockResolvedValue(userMock)
+      expect(await repository.getById('any_id')).toEqual(userMock)
+    })
+  })
+})
